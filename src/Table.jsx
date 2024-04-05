e4abda0fe31dce5961ebc47a7e3edc84faaba160
IvanTicona <ivan.ticona.v@gmail.com>
2024-04-04 21:53:19 -0400

    Calculando complejidad

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,9 +1,39 @@
 /* eslint-disable no-unused-vars */
-import sudoku from "./Sudoku";
+// import sudoku from "./Sudoku";
 
 export const Table = () => {
   const nxm = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
+  let sudoku = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+  ];
+
+  const fillSudoku = () => {
+    
+
+    for (let i = 0; i < sudoku.length; i++) {
+      for (let j = 0; j < sudoku.length; j++) {
+        while(sudoku[i].length < 9){
+          const rand = Math.floor(Math.random()*9) + 1
+          if (!sudoku[i].includes(rand)) {
+            sudoku[i].push(rand)
+          }
+        }
+      }
+    }
+
+  };
+   
+  fillSudoku();
+  
   return (
     <div className="sudoku">
       {nxm.map((row) => {
